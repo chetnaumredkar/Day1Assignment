521ef2b27dc1507e10fb03d5feef7245a1a6ac88
kolhechetna <kolhe.chetna@gmail.com>
2022-11-05 07:38:16 +0530

    Added:Primitives Data types Assignment

---
+++ src/com/bridgelabz/PDataTypes.java
@@ -0,0 +1,34 @@
+package com.bridgelabz;
+
+public class PDataTypes {
+	public static void main(String[] args) {
+		byte b = 20;
+		System.out.println("b= " + b);
+
+		// short type
+		short s = 20;
+		System.out.println("s= " + s);
+
+		// int type
+		int i = 20;
+		System.out.println("i= " + i);
+
+		// long type
+		long l = 20;
+		System.out.println("l= " + l);
+		float f = 20.25f;
+		System.out.println("f= " + f);
+
+		// double type
+		double d = 20.25;
+		System.out.println("d= " + d);
+		// character type
+		char ch = 'S';
+		System.out.println(ch);
+		// boolean type
+		boolean t = true;
+		System.out.println(t);
+
+	}
+
+}
