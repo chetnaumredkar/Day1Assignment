9e9d8384c2d94d83f245a36350cb4137b02614f6
kolhechetna <kolhe.chetna@gmail.com>
2022-11-05 07:40:36 +0530

    Added:To check two strings are equal or not.

---
+++ src/com/bridgelabz/TwoString.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+public class TwoString {
+	public static void main(String[] args) {
+		String s1 = "aarti";
+		String s2 = "banana";
+		if (s1.equals(s2))
+			System.out.println("Equal CHARACTER");
+		else
+			System.out.println("Unequal chARACTER");
+
+	}
+
+}
